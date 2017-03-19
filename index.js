089cfabddc9e1b2a61963bfb5db0a3e66f3ac424
Akomi <antprm@mail.ru>
2017-03-19 16:44:28 +0500

    ответ на все сообщения

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 const got = require('got');
 const baseUrl = 'https://api.telegram.org/bot343581946:AAENXqqX354avRrDK4NYll5zOVL2XJjz8bQ/';
-const upd = 'getUpdates';
-var updId;
-
-function lastUpdId(response) {
-    var data;
-    try {
-        data = JSON.parse(response.body);
-    } catch (error) {
-        console.log('не JSON');
-        console.log(error);
-        clearInterval(timer);
-        return;
-    }
-    updId = data.result[data.result.length - 1].update_id;
-}
 
 function resultP1(response) {
     var data;
@@ -25,34 +10,37 @@ function resultP1(response) {
         clearInterval(timer);
         return;
     }
-   
-    var mess = data.result[data.result.length - 1];
-    if (mess.update_id > updId && mess.message) {
+
+    var mess = data.result[0];
+    if (mess === undefined){
+        return;
+    }
+
+    var lastUpdId = data.result[data.result.length - 1].update_id;
+    var firstUpdId = data.result[0].update_id;
+
+    if (firstUpdId <= lastUpdId && mess.message) {
         console.log(mess.message.from.first_name + ": " + mess.message.text);
-        var chatId = data.result[data.result.length - 1].message.chat.id;
+        var chatId = mess.message.chat.id;
         var lastMessage = mess.message.text;
-        p2(chatId, lastMessage);
-        updId = mess.update_id;
-        clear(updId);
+        sendMessage(chatId, lastMessage);
+        clear(firstUpdId);
     }
 }
 
-function p2(chatId, lastMessage) {
+function sendMessage(chatId, lastMessage) {
     lastMessage = encodeURI(lastMessage);
     var send = 'sendMessage?chat_id=' + chatId + '&text=' + lastMessage;
     got(baseUrl + send);
 }
 
-function clear(updId) {
-    updId = updId - 10;
-    got(baseUrl + upd + '?offset=' + updId);
+function clear(firstUpdId) {
+    firstUpdId = firstUpdId + 1;
+    got(baseUrl + 'getUpdates?offset=' + firstUpdId);
 }
 
-got(baseUrl + upd)
-    .then(lastUpdId);
-
 function main() {
-    got(baseUrl + upd)
+    got(baseUrl + 'getUpdates')
         .then(resultP1);
 }
 
